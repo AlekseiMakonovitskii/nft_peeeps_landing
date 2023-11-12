48eeb913bfbd283336a88503cc934b01a9391d5a
AlekseiMakonovitskii <alexmakondev@gmail.com>
2023-11-12 19:57:03 +0300

    added header plus app styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from '../scss/Header.module.scss'
+import logo from '../assets/logo.svg'
+import burger from '../assets/burger.svg'
 
 const Header = () => {
 	return (
 		<header className={styles.header}>
-			f
+			<a href=""><img src={logo} alt="" /></a>
+			<a href=""><img src={burger} alt="" /></a>
 		</header>
 	)
 }
