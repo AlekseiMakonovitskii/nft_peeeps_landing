3187404695e35ad30e446d5445d38378436b458e
AlekseiMakonovitskii <alexmakondev@gmail.com>
2023-11-14 20:52:01 +0300

    created collection section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Collection from './components/Collection'
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
       <Hero />
-      
+      <Collection/>
     </div>
   )
 }
---
+++ src/components/Collection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../scss/Collection.module.scss'
+
+const Collection = () => {
+	return (
+		<section className={styles.collection}>
+			collection
+		</section>
+	)
+}
+
+export default Collection
