70a9efa81e6d87fda78c1752c4ba8ab7a5d9e58e
AlekseiMakonovitskii <alexmakondev@gmail.com>
2023-11-14 20:33:59 +0300

    added hero section partly

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,25 +4,32 @@ import oldMan from '../assets/oldMan.svg';
 import woman from '../assets/woman.svg';
 import lightCircle from '../assets/lightCircle.svg';
 import pinkCircle from '../assets/pinkCircle.svg';
+import bubbles from '../assets/bubbles.svg';
+import bubbles2 from '../assets/bubbles2.svg';
+import titleBubble from '../assets/titleBubble.svg';
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.text}>
-        <h3>NFT Collection</h3>
-        <h1>
-          Bored <span>NFT Peeps</span>
-        </h1>
+        <div className={styles.littleTitle}>
+          <img src={titleBubble} alt="" className={styles.titleBubble} />
+          <h3>NFT Collection</h3>
+        </div>
+
+        <div className={styles.mainTitle}>
+          <h1>
+            Bored <span>NFT Peeps</span>
+          </h1>
+  
+        </div>
       </div>
-      <div className={styles.images}>
-        <div className={styles.imageContainer}>
-          <img src={oldMan} alt="" className={styles.oldMan} />
-          <img src={lightCircle} alt="" className={styles.lightCircle} />
-        </div>
-        <div className={styles.imageContainer}>
-          <img src={woman} alt="" className={styles.woman} />
-          <img src={pinkCircle} alt="" className={styles.pinkCircle} />
-        </div>
+
+
+
+
+      <div className={styles.randomBubbles}>
+
       </div>
     </section>
   );
